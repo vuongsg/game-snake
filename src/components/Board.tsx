202e4bade52bf823272e4db9dd9b2ef0d272cf85
vuongsg <vuong.goteborg@gmail.com>
2021-09-21 15:50:17 +0700

    Move, eat food

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,37 +1,134 @@
 import { Container, Grid } from "@material-ui/core";
-import React, { ReactElement, useRef } from "react";
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Constants } from "../constants";
-import { BoardState } from "../slices/board-slice";
+import { BoardState, Direction, enhanceScore, reset, changeDirection, changePosition, start } from "../slices/board-slice";
 import { RootType } from "../store";
 import '../styles/Board.scss';
 
-export interface BoardParameter {
-    targetRow: number;
-    targetCol: number;
-}
+export const Board = () => {
+    let { score, currentRow, currentCol, targetRow, targetCol, direction, isDelayed } = useSelector<RootType>(state => state.Board) as BoardState;
+    const dispatch = useDispatch();
+    const board: number[][] = Array(Constants.SIZE_BOARD);
 
-export const Board: React.FC<BoardParameter> = (props) => {
-    const score = (useSelector<RootType>(state => state.Board) as BoardState).score;
-    const board = (useSelector<RootType>(state => state.Board) as BoardState).board;
+    console.log(`current row: ${currentRow}`);
+    console.log(`current col: ${currentCol}`);
+    console.log(`target row: ${targetRow}`);
+    console.log(`target col: ${targetCol}`);
+    console.log(`direction: ${direction}`);
+    console.log(`delay: ${isDelayed}`);
 
-    return (
-        <Container className='main-container'>
-            <Grid item xs={12}>
-                <h1>Score: {score}</h1>
-            </Grid>
+    const timeout = setInterval(() => {
+        if (isDelayed || direction === Direction.None) return;
 
-            <Grid container direction='row' id='grid-board'>{
-                <Grid item lg={12}>{
-                    board.map((row, idx) =>
-                    <div className='row-board'>{
-                        row.map((col, i) =>
-                            <div className='cell-board'></div>
-                    )}</div>
-                )}
-                </Grid>
+        switch (direction) {
+                case Direction.Up:
+                    currentRow--;
+                    break;
+                case Direction.Down:
+                    currentRow++;
+                    break;
+                case Direction.Left:
+                    currentCol--;
+                    break;
+                case Direction.Right:
+                    currentCol++;
+                    break;
             }
-            </Grid>
-        </Container>
-    )
-}
+
+            checkBoardState();
+    }, 120);
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+             document.removeEventListener('keydown',  handleKeyDown);
+             clearInterval(timeout);
+        }
+    }, []);
+
+    /* useEffect(() => {
+        const timeout = setInterval(() => {
+            if (isDelayed || direction === Direction.None) return;
+
+        switch (direction) {
+                case Direction.Up:
+                    if (currentRow > 0) currentRow--;
+                    break;
+                case Direction.Down:
+                    if (currentRow < Constants.SIZE_BOARD) currentRow++;
+                    break;
+                case Direction.Left:
+                    if (currentCol > 0) currentCol--;
+                    break;
+                case Direction.Right:
+                    if (currentCol < Constants.SIZE_BOARD) currentCol++;
+                    break;
+            }
+
+            checkBoardState();
+        }, 120);
+
+        return () => clearInterval(timeout);
+    }, []); */
+
+    const initializeBoard = (): void => { for (let i = 0; i < Constants.SIZE_BOARD; i++) board[i] = Array(Constants.SIZE_BOARD).fill(0); }
+    initializeBoard();
+
+    const getCellClassName = (rowIdx: number, colIdx: number): string => {
+        let className = `cell-board ${rowIdx}-${colIdx}`;
+        if (rowIdx === currentRow && colIdx === currentCol) className += ' current';
+        else if (rowIdx === targetRow && colIdx === targetCol) className += ' target';
+
+        return className;
+    }
+
+    const checkBoardState = () => {
+        if (currentRow >= 0 && currentRow < Constants.SIZE_BOARD && currentCol >= 0 && currentCol < Constants.SIZE_BOARD)
+            dispatch(changePosition([currentRow, currentCol]));
+
+        if (currentRow === targetRow && currentCol === targetCol) {
+            dispatch(enhanceScore([currentRow, currentCol, targetRow, targetCol]));
+        } else if (currentRow <= 0 || currentRow >= Constants.SIZE_BOARD - 1 || currentCol <= 0 || currentCol >= Constants.SIZE_BOARD - 1) {
+            dispatch(reset());
+        }
+    }
+
+    const handleKeyDown = (e: globalThis.KeyboardEvent): void => {
+        switch (e.key) {
+            case 'ArrowUp':
+                direction = Direction.Up;
+                break;
+            case 'ArrowDown':
+                direction = Direction.Down;
+                break;
+            case 'ArrowLeft':
+                direction = Direction.Left;
+                break;
+            case 'ArrowRight':
+                direction = Direction.Right;
+                break;
+        }
+
+        if (isDelayed) dispatch(start(direction));
+        else dispatch(changeDirection(direction));
+    }
+
+return (
+    <Container className='main-container'>
+        <Grid item xs={12}>
+            <h1>Score: {score}</h1>
+        </Grid>
+
+        <Grid item xs={12} id='grid-board'>{
+            board.map((row, rowIdx) =>
+            <div key={rowIdx} className='row-board'>{
+                row.map((cell, colIdx) => {
+                    let className = getCellClassName(rowIdx, colIdx);
+                    return <div key={colIdx} className={className} tabIndex={0}></div>
+                })}</div>
+            )}
+        </Grid>
+    </Container>
+)}
--- src/slices/board-slice.ts
+++ src/slices/board-slice.ts
@@ -1,24 +1,104 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Constants } from "../constants";
 
+export enum Direction {
+    None ='NONE',
+    Up = 'UP',
+    Down = 'DOWN',
+    Left = 'LEFT',
+    Right = 'RIGHT'
+}
+
 export interface BoardState {
     score: number,
-    board: boolean[][]
+    currentRow: number,
+    currentCol: number,
+    targetRow: number,
+    targetCol: number,
+    direction: Direction,
+    isDelayed: boolean
 }
 
 const initialState: BoardState = {
     score: 0,
-    board: Array(Constants.SIZE_BOARD)
+    currentRow: 2,
+    currentCol: 2,
+    targetRow: 10,
+    targetCol: 10,
+    direction: Direction.Down,
+    isDelayed: true
 }
 
-for (let i = 0; i < Constants.SIZE_BOARD; i++) initialState.board[i] = Array(15).fill(false);
+/**
+ * Return [targetRow, targetCol]
+ * @param currentRow 
+ * @param currentCol 
+ */
+const setTargets = (currentRow: number, currentCol: number): [number, number] => {
+    let targetRow = -1, targetCol = -1;
+
+    do {
+        targetRow = Math.floor(Math.random() * (Constants.SIZE_BOARD - 1));
+        targetCol = Math.floor(Math.random() * (Constants.SIZE_BOARD - 1));
+
+        if (targetRow === 0) targetRow = 1;
+        else if (targetRow === Constants.SIZE_BOARD - 1) targetRow = Constants.SIZE_BOARD - 2;
+
+        if (targetCol === 0) targetCol = 1;
+        else if (targetCol === Constants.SIZE_BOARD - 1) targetCol = Constants.SIZE_BOARD - 2;
+    } while (currentRow === targetRow && currentCol === targetCol);
+
+    return [targetRow, targetCol];
+}
 
 const slice = createSlice({
     name: 'board',
     initialState: initialState,
-    reducers: {
-        
+    reducers: { //PayloadAction<[currentRow, currentCol, targetRow, targetCol]>
+        enhanceScore: (state: BoardState, action: PayloadAction<[number, number, number, number]>) => {
+            const targets = setTargets(action.payload[0], action.payload[1]);
+
+            return {
+                ...state,
+                score: state.score + 1,
+                targetRow: targets[0],
+                targetCol: targets[1]
+            }
+        },
+        reset: (state: BoardState) => {
+            return {
+                ...state,
+                score: 0,
+                currentRow: 2,
+                currentCol: 2,
+                targetRow: 10,
+                targetCol: 10,
+                direction: Direction.None,
+                isDelayed: true
+            }
+        },
+        changeDirection: (state: BoardState, action: PayloadAction<Direction>) => {
+            return {
+                ...state,
+                direction: action.payload
+            }
+        },
+        changePosition: (state: BoardState, action: PayloadAction<[number, number]>) => {
+            return {
+                ...state,
+                currentRow: action.payload[0],
+                currentCol: action.payload[1],
+            }
+        },
+        start: (state: BoardState, action: PayloadAction<Direction>) => {
+            return {
+                ...state,
+                isDelayed: false,
+                direction: action.payload
+            }
+        }
     }
 });
 
+export const { enhanceScore, reset, changeDirection, changePosition, start } = slice.actions;
 export default slice.reducer;
